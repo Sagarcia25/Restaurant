417319487f41427bb757e4b8facf8787ff0a8019
Salvador <Sagarcia2713@gmail.co>
2019-04-14 16:57:36 -0500

    In the CategoryTableViewController file, the viewDidLoad, data is fetched in order to display in the tableView. A function updateUI was added to update the tableView.

--- Restaurant/Controllers/CategoryTableViewController.swift
+++ Restaurant/Controllers/CategoryTableViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class CategoryTableViewController: UITableViewController {
 
+    let menuController = MenuController()
+    var categories = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        menuController.fetchCategories{ (categories) in
+            if let categories = categories{
+                self.updateUI(with: categories)
+            }
+        }
     }
-
+    
+    func updateUI(with categories: [String]) {
+        DispatchQueue.main.async {
+            self.categories = categories
+            self.tableView.reloadData()
+        }
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
