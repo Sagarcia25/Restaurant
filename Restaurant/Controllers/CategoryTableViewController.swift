7723715c58c9201f37c9020bede675e7d66d46ed
Salvador <Sagarcia2713@gmail.co>
2019-04-16 19:23:17 -0500

    Within the CategoryTableView file,  I changed the numberOfSections table view function to return 1 section. In numberOfRowsInSection, I am returning only rows equal to the amount of category objects. The cells within the table view are being recycled with the CategoryCellIdentifier name in the cellForRowAt function. A function named configure  was created to configure the cell's text label to the category's name.

--- Restaurant/Controllers/CategoryTableViewController.swift
+++ Restaurant/Controllers/CategoryTableViewController.swift
@@ -33,24 +33,28 @@ class CategoryTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return categories.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCellIdentifier", for: indexPath)
 
-        // Configure the cell...
+        configure(cell, forItemAt: indexPath)
 
         return cell
     }
-    */
-
+    
+    func configure(_ cell: UITableViewCell, forItemAt indexPath: IndexPath) {
+        let categoryString = categories[indexPath.row]
+        cell.textLabel?.text = categoryString.capitalized
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
