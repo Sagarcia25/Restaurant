11a7346677c67387466c04bb3d894533ed8fafed
Salvador <Sagarcia2713@gmail.co>
2019-04-18 17:52:54 -0500

    On the main storyboard, the cell on the orderTableViewController was given the reusable cell identifier OrderCellIndentifier. In OrderTableViewController file, a variable named order was defined to hold Order object when passed from the MenuItemDetailViewController. Another configure function was made to update the cell with Order object properties. The tableview will then populate with order items that the user adds.

--- Restaurant/Controllers/OrderTableViewController.swift
+++ Restaurant/Controllers/OrderTableViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class OrderTableViewController: UITableViewController {
+    
+    var order = Order()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,24 +26,29 @@ class OrderTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return order.menuItems.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OrderCellIdentifier", for: indexPath)
 
-        // Configure the cell...
+        configure(cell, forItemAt: indexPath)
 
         return cell
     }
-    */
+ 
 
+    func configure(_ cell: UITableViewCell, forItemAt indexPath: IndexPath) {
+        let menuItem = order.menuItems[indexPath.row]
+        cell.textLabel?.text = menuItem.name
+        cell.detailTextLabel?.text = String(format: "$%.2f", menuItem.price)
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
