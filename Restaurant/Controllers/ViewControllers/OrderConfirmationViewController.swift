fbb31148b880aa9f334f697253f3916ed7c4618d
Salvador <Sagarcia2713@gmail.co>
2019-04-18 21:58:49 -0500

    Added a right bar button item on the confirmation storyboard named submit so that the user may confirm their order. Connected the bar button to the OrderConfirmationViewController. The IBAction, submitTapped, will display an alert to the user about the total cost of the order. The user will then need to either confirm or cancel the order.

--- Restaurant/Controllers/ViewControllers/OrderConfirmationViewController.swift
+++ Restaurant/Controllers/ViewControllers/OrderConfirmationViewController.swift
@@ -12,6 +12,7 @@ class OrderConfirmationViewController: UIViewController {
 
     @IBOutlet weak var timeRemainingLabel: UILabel!
     var minutes: Int!
+    var orderMinutes = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +20,34 @@ class OrderConfirmationViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func submitTapped(_ sender: Any) {
+        let orderTotal = MenuController.shared.order.menuItems.reduce(0.0){
+            (result, menuItem) -> Double in
+            return result + menuItem.price
+        }
+        
+        let formattedOrder = String(format: "$%.2f", orderTotal)
+        
+        let alert = UIAlertController(title: "Confirm Order", message: "You are about to submit your order with a total of \(formattedOrder)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Submit", style: .default){ action in
+            self.uploadOrder()
+        })
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func uploadOrder(){
+        let menuIds = MenuController.shared.order.menuItems.map { $0.id}
+        MenuController.shared.submitOrder(forMenuIds: menuIds){
+            (minutes) in
+            DispatchQueue.main.async {
+                if let minutes = minutes{
+                    self.orderMinutes = minutes
+                    self.performSegue(withIdentifier: "ConfirmationSegue", sender: nil)
+                }
+            }
+        }
+    }
     /*
     // MARK: - Navigation
 
